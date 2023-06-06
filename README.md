c250ac3542aec02f29beb3c3bda9da543d38bb11
Robson Inocêncio <robson.inocencio@gmail.com>
2023-06-06 09:07:12 -0300

    feat: initial config

---
+++ README.md
@@ -0,0 +1,5 @@
+yarn init -y
+yarn add express
+yarn add -D typescript @types/express ts-node-dev
+
+yarn ts-node-dev ./src/index.ts
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { server } from "./server/Server";
+
+const PORT = 3333;
+
+server.listen(PORT, () => {
+  console.log("Server started on port = " + PORT);
+});
