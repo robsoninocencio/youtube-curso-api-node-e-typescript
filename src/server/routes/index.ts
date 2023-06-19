54af16c2fab228458c5244140ee0be35f031f68f
Robson Inocêncio <robson.inocencio@gmail.com>
2023-06-19 01:55:57 -0300

    feat: Criando controller, tipando entrada de dados

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,29 +1,15 @@
 import { Router } from 'express';
-import { StatusCodes } from 'http-status-codes';
+
+import { CidadesController } from './../controllers';
 
 const router = Router();
 
-router.get('/recurso', (_, res) => {
+router.get('/', (_, res) => {
   return res.send(
     'Olá, DEV! Este é um exemplo de rota de leitura, método http GET'
   );
 });
 
-router.post('/recurso', (req, res) => {
-  console.log(req.body);
-  return res.status(StatusCodes.CREATED).json(req.body);
-});
-
-router.put('/recurso', (req, res) => {
-  return res.json({
-    texto: 'Olá, DEV! Este é um exemplo de rota alteração, método http PUT',
-  });
-});
-
-router.delete('/recurso', (req, res) => {
-  return res.json({
-    texto: 'Olá, DEV! Este é um exemplo de rota exclusão, método http DELETE',
-  });
-});
+router.post('/cidades', CidadesController.create);
 
 export { router };
