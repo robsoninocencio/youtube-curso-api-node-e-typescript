ab9dc3237d17890301a9b060f77cd3278dff1019
Robson Inocêncio <robson.inocencio@gmail.com>
2023-06-19 14:12:22 -0300

    feat: Tradução do Yup e validação de múltiplos erros

--- src/server/controllers/cidades/create.ts
+++ src/server/controllers/cidades/create.ts
@@ -4,27 +4,36 @@ import * as yup from 'yup';
 
 interface ICidade {
   nome: string;
+  estado: string;
 }
 
 const bodyValidation: yup.Schema<ICidade> = yup.object().shape({
   nome: yup.string().required().min(3),
+  estado: yup.string().required().min(2),
 });
 
 export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
   let validatedData: ICidade | undefined = undefined;
 
   try {
-    validatedData = await bodyValidation.validate(req.body);
-  } catch (error) {
-    const yupError = error as yup.ValidationError;
-    // console.error(yupError);
-    return res.status(StatusCodes.BAD_REQUEST).json({
-      errorerrors: {
-        default: yupError.message,
-      },
+    validatedData = await bodyValidation.validate(req.body, {
+      abortEarly: false,
     });
+  } catch (err) {
+    const yupError = err as yup.ValidationError;
+    const errors: Record<string, string> = {};
+
+    if (yupError.inner) {
+      yupError.inner.forEach((error) => {
+        if (error.path === undefined) return;
+        errors[error.path] = error.message;
+      });
+    }
+
+    return res.status(StatusCodes.BAD_REQUEST).json({ errors });
   }
 
   console.log(validatedData);
+
   return res.status(StatusCodes.CREATED).send('Create!');
 };
