1963cca0cc915fa14f91099ca4896bbc85ac2669
Robson Inocêncio <robson.inocencio@gmail.com>
2023-06-24 19:22:33 -0300

    feat: Finalizando controller de cidades

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -16,10 +16,28 @@ router.get(
   CidadesController.getAll
 );
 
+router.get(
+  '/cidades/:id',
+  CidadesController.getByIdValidation,
+  CidadesController.getById
+);
+
 router.post(
   '/cidades',
   CidadesController.createValidation,
   CidadesController.create
+);
+
+router.put(
+  '/cidades/:id',
+  CidadesController.updateByIdValidation,
+  CidadesController.updateById
+);
+
+router.delete(
+  '/cidades/:id',
+  CidadesController.deleteByIdValidation,
+  CidadesController.deleteById
 );
 
 export { router };
