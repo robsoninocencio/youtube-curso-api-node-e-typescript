4cfb91d143f139505a6fd2d74d7028f65495c7ce
Robson Inocêncio <robson.inocencio@gmail.com>
2023-07-09 23:47:29 -0300

    feat: Criando provider para cidade

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -4,13 +4,14 @@ import * as yup from 'yup';
 
 import { validation } from '../../shared/middleware';
 import { ICidade } from '../../database/models';
+import { CidadesProvider } from '../../database/providers/cidades';
 
 interface IBodyProps extends Omit<ICidade, 'id'> {}
 
 export const createValidation = validation((getSchema) => ({
   body: getSchema<IBodyProps>(
     yup.object().shape({
-      nome: yup.string().required().min(3),
+      nome: yup.string().required().min(3).max(150),
     })
   ),
 }));
@@ -19,5 +20,14 @@ export const create = async (
   req: Request<{}, {}, IBodyProps>,
   res: Response
 ) => {
-  return res.status(StatusCodes.CREATED).json(1);
+  const result = await CidadesProvider.create(req.body);
+
+  if (result instanceof Error) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      errors: {
+        default: result.message,
+      },
+    });
+  }
+  return res.status(StatusCodes.CREATED).json(result);
 };
