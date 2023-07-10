36c887ee5b29910c7855c6e83916d21ac2d4e716
Robson Inocêncio <robson.inocencio@gmail.com>
2023-07-10 01:17:30 -0300

    feat: Finalizando provider de cidade

--- src/server/database/providers/cidades/index.ts
+++ src/server/database/providers/cidades/index.ts
@@ -1,13 +1,15 @@
+import * as deleteById from './DeleteById';
+import * as updateById from './UpdateById';
+import * as getById from './GetById';
 import * as create from './Create';
-// import * as getAll from './GetAll';
-// import * as getById from './GetById';
-// import * as updateById from './UpdateById';
-// import * as deleteById from './DeleteById';
+import * as getAll from './GetAll';
+import * as count from './Count';
 
 export const CidadesProvider = {
+  ...deleteById,
+  ...updateById,
+  ...getById,
   ...create,
-  // ...getAll,
-  // ...getById,
-  // ...updateById,
-  // ...deleteById,
+  ...getAll,
+  ...count,
 };
